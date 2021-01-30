a16be2d905f8fdb6e8aa550fe78af6abde3b6f2c
Martín <martin@animadata.com>
2021-01-30 08:04:46 -0300

    Nuevo commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace netnivel2
+{
+    class Program
+    {
+
+        struct Persona
+        {
+            public string Nombre;
+            public string Apellido;
+            public int Edad;
+        }
+
+
+        static void Main()
+        {
+            Persona Martin = new Persona();
+            Martin.Nombre = "Martín";
+            Martin.Apellido = "Longo";
+            Martin.Edad = 30;
+            Console.WriteLine(Martin.Nombre);
+            Console.WriteLine(Martin.Apellido);
+        }
+    }
+}
